d980dcf0d57557892bf6fe957f80a093770673c0
zetsubranco <augustoreisgyn80@gmail.com>
2023-06-12 19:35:06 -0300

    calc-add

--- calc.py
+++ calc.py
@@ -1,1 +1,28 @@
+from random import randint
+import os
+import time
+
 print("----\njogo da tabuada\n----")
+
+def tabuada():
+    jogar = int(input("Com qual tabuada você quer jogar? "))
+    while True: 
+        
+        numero = randint(1, 10)
+        pergunta = int(input("Quanto é {} x {}? ".format(jogar, numero)))
+        if pergunta != jogar * numero:
+            option = input("Você errou. 's' para sair - 'a' para jogar de novo ")
+            if option == 's':
+                 break
+            elif option == 'a':
+                tabuada()
+
+           
+        else:
+            print("você acertou!")
+            time.sleep(1)
+            os.system("cls")
+        
+
+tabuada()
+
